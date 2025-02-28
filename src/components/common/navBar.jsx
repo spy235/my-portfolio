a06093e3eeed5ae6bffc78115a84dfad8aa1381e
yashasj <yashas.j@dbizsolution.com>
2025-02-28 18:20:49 +0530

    first commit

--- src/components/common/navBar.jsx
+++ src/components/common/navBar.jsx
@@ -19,7 +19,7 @@ const NavBar = (props) => {
 										: "nav-item"
 								}
 							>
-								<Link to="/">Home</Link>
+								<Link to="/my-portfolio">Home</Link>
 							</li>
 							<li
 								className={
